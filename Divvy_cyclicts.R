ce52331316df0e523fbd40aaefa90310e338988e
KadambiKashyap <108693785+KadambiKashyap@users.noreply.github.com>
2022-07-09 03:07:48 +0200

    Update Divvy_cyclicts.R

--- Divvy_cyclicts.R
+++ Divvy_cyclicts.R
@@ -36,27 +36,29 @@ colnames(q2_2019)
 
 # Renaming columns to make them consistent with q1_2020
 
-(q2_2019 <-rename(q2_2019
-                  ,ride_id = "X01...Rental.Details.Rental.ID"                    
-                  ,started_at = "X01...Rental.Details.Local.Start.Time"            
-                  ,ended_at = "X01...Rental.Details.Local.End.Time"               
-                  ,rideable_type = "X01...Rental.Details.Bike.ID"                     
-                  ,start_station_id = "X03...Rental.Start.Station.ID"                    
-                  ,start_station_name = "X03...Rental.Start.Station.Name"                   
-                  ,end_station_id = "X02...Rental.End.Station.ID"                      
-                  ,end_station_name = "X02...Rental.End.Station.Name"                     
-                  ,member_casual = "User.Type"))
+(q2_2019 <- rename(q2_2019
+                   ,ride_id = "01 - Rental Details Rental ID"
+                   ,rideable_type = "01 - Rental Details Bike ID" 
+                   ,started_at = "01 - Rental Details Local Start Time"  
+                   ,ended_at = "01 - Rental Details Local End Time"  
+                   ,start_station_name = "03 - Rental Start Station Name" 
+                   ,start_station_id = "03 - Rental Start Station ID"
+                   ,end_station_name = "02 - Rental End Station Name" 
+                   ,end_station_id = "02 - Rental End Station ID"
+                   ,member_casual = "User Type"))
 
-(q3_2019 <-rename(q3_2019
-                  ,ride_id = "trip_id"
-                  ,started_at = "start_time"
-                  ,ended_at = "end_time"
-                  ,rideable_type = "bikeid"
-                  ,start_station_id = "from_station_id"
-                  ,start_station_name = "from_station_name"
-                  ,end_station_id = "to_station_id"
-                  ,end_station_name = "to_station_name"  
-                  ,member_casual = "usertype"))
+
+(q3_2019 <- rename(q3_2019
+                   ,ride_id = "trip_id"
+                   ,rideable_type = "bikeid" 
+                   ,started_at = "start_time"  
+                   ,ended_at = "end_time"  
+                   ,start_station_name = "from_station_name" 
+                   ,start_station_id = "from_station_id" 
+                   ,end_station_name = "to_station_name "
+                   ,end_station_id = "to_station_id" 
+                   ,member_casual = "usertype"))
+
 
 (q4_2019 <-rename(q4_2019
                   ,ride_id = "trip_id"
@@ -79,14 +81,15 @@ str(q1_2020)
 
 # Convert ride_id and rideable_type to character so that they can stack correctly
 
-q4_2019 <-mutate(q4_2019, ride_id = as.character(ride_id),
-                 rideable_type = as.character(rideable_type))
+q4_2019 <- mutate(q4_2019, ride_id = as.character(ride_id)
+                   ,rideable_type = as.character(rideable_type)) 
 
-q3_2019 <-mutate(q3_2019, ride_id = as.character(ride_id),
-                 rideable_type = as.character(rideable_type))
+q3_2019 <- mutate(q3_2019, ride_id = as.character(ride_id)
+                   ,rideable_type = as.character(rideable_type)) 
 
-q2_2019 <-mutate(q2_2019, ride_id = as.character(ride_id),
-                 rideable_type = as.character(rideable_type))
+q2_2019 <- mutate(q2_2019, ride_id = as.character(ride_id)
+                   ,rideable_type = as.character(rideable_type)) 
+
 
 
 # appending data
